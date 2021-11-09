d8adbe6a060f554a8920327b2b11433232c63ead
nikita.kochetkov <nikita.kochetkov@jetbrains.com>
2021-11-09 03:41:28 +0300

    save state

--- archiver/src/main/kotlin/com.kochetkov.archiver/Solve.kt
+++ archiver/src/main/kotlin/com.kochetkov.archiver/Solve.kt
@@ -3,6 +3,7 @@ package com.kochetkov.archiver
 import java.io.File
 import java.io.FileOutputStream
 import java.lang.Math.pow
+import java.nio.charset.StandardCharsets
 import kotlin.math.log2
 import kotlin.math.pow
 
@@ -36,7 +37,6 @@ data class Solve(val mode:String, val input: File, val output: File) {
         println("deco mtf: ${decodeMtf(mtf)}")
 
 
-
         FileOutputStream(output).use { writer ->
             mtf.intList.forEach {
                 val charCode = it.toString().first().code
@@ -52,6 +52,57 @@ data class Solve(val mode:String, val input: File, val output: File) {
                 writer.write(res.toByteArray())
             }
         }
+
+//        var allText = ""
+        println("try decode")
+        var counter = 0
+        val resMessage = StringBuilder()
+        output.bufferedReader().use { reader ->
+            while(true) {
+                val readValue = reader.read()
+                if (readValue == -1) {
+                    break
+                }
+                val char = readValue.toChar()
+                if (char == '1') {
+                    counter++
+                } else {
+                    val f1 = pow(2.0, counter.toDouble())
+                    val sb = StringBuilder()
+                    while (counter > 0) {
+                        sb.append(reader.read().toChar())
+                        counter--
+                    }
+                    val sum = f1.toInt() + Integer.getInteger(sb.toString(), 2)
+                    println("d1: ${f1.toInt()}   d2: ${Integer.getInteger(sb.toString(), 2)}")
+
+                    val wasChar = sum.toChar()
+                    resMessage.append(wasChar)
+                    println("wasChar: $wasChar")
+                    println("wasChar int: ${wasChar.toInt()}")
+                    //sec = (charCode - 2.0.pow(tmp).toInt()).toString(2)
+//                    val firstPart = log2(charCode.toFloat()).toInt()
+//                    val firstPart2 = log2(charCode.toFloat()).toInt()
+                    counter = 0
+
+                }
+            }
+//            allText = reader.readText()
+        }
+
+        println("decoded MESSAGE: ${resMessage}")
+
+//        for (char in allText) {
+//            if (char == '1') {
+//                counter++
+//                continue
+//            } else {
+//                val f1 = pow(2.0, counter.toDouble())
+//                counter = 0
+//
+//            }
+//        /
+
     }
 
     private fun mtf(text: String): MTF {
