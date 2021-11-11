39633ff44870a641c1fa04c3bf72191334b99f3c
nikita.kochetkov <nikita.kochetkov@jetbrains.com>
2021-11-11 21:49:33 +0300

    work for encode

--- archiver/src/main/kotlin/com.kochetkov.archiver/app/Main.kt
+++ archiver/src/main/kotlin/com.kochetkov.archiver/app/Main.kt
@@ -7,26 +7,21 @@ class Main {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            if (args.size < 3) {
-                System.err.println("Expected tree arguments: <decode|encode> <input> <output>")
+            if (args.size < 2) {
+                System.err.println("Expected tree arguments: <input> <output>")
                 return
             }
 
-            val mode = args[0]
-            val inputFile = File(args[1])
-            val outputFile = File(args[2])
-
-            if (mode != "encode" && mode != "decode") {
-                System.err.println("Invalid mode used: <decode|encode> <input> <output>")
-                return
-            }
+            val inputFile = File(args[0])
+            val outputFile = File(args[1])
 
             if (!inputFile.exists()) {
                 System.err.println("Input file doesn't exist")
                 return
             }
 
-            Solve(mode, inputFile, outputFile).solve()
+            Solve("encode", inputFile, outputFile).solve()
+//            Solve("decode", inputFile, outputFile).solve()
         }
     }
 }
